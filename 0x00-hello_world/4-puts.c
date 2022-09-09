ee815f82008c0d8e894784cd6629b922349928d3
Sannithebaba <mubaraksanniajiferuke@gmail.com>
2022-09-09 14:47:30 -0700

    positive anything

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+
 /**
+ * main - print the string in the put function
+ * 
+ * Description: using the main function
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+
 int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
 /**
+ *  main - Print the string in the put function
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
-
+ *  Description using the main function
+ *  this program prints "Programming is like building a multilingual puzzle
+ *  Return:0
+ */
 int main(void)
-
 {
 
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
-
 }
