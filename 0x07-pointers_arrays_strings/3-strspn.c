1b8ad6ac8535c0a63214f237e1973df2d2bddff1
Sannithebaba <mubaraksanniajiferuke@gmail.com>
2022-09-26 06:45:26 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,7 +8,7 @@
  * Return: number of bytes in the initial segment of s coninting accept bytes
  */
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
 {
 	int j;
 	unsigned int count = 0;
