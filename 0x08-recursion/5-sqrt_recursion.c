95c200fb4ca38fb0e0a9922a804a30ac8da75ddc
Sannithebaba <mubaraksanniajiferuke@gmail.com>
2022-09-28 11:38:23 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -17,3 +17,29 @@ int _sqrt_recursion(int n)
 
 	return (_fact(n, 1));
 }
+
+/**
+ * _fact - gets the factor by which result equals the factor
+ * @num: number to be sequentialy divided
+ * @fact: factor to be evaluated and increased if needed
+ *
+ * Return: square root of num, or -1 if not found
+ */
+
+int _fact(int num, int fact)
+{
+	int r = 0;
+
+	if ((num / fact == fact) && (num % fact == 0))
+		return (1);
+
+	if (num / fact == 0)
+		return (-1);
+
+	r = _fact(num, fact + 1);
+
+	if (r < 0)
+		return (-1);
+
+	return (1 + r);
+}
