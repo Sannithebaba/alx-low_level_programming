93888f16dbd313862c4988029aeef46819c1c719
Sannithebaba <mubaraksanniajiferuke@gmail.com>
2022-10-06 12:49:05 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * _calloc - allocates memory for an array
  * @nmemb: number of elements in array
@@ -8,39 +8,20 @@
  *
  * Return: a void pointer to the allocated memory
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = NULL;
+	char *arr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
 
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
 		return (NULL);
-	setm_zero((char *)ptr, size * nmemb);
 
-	return (ptr);
-}
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
 
-
-/**
- * setm_zero - set memory on zero
- * @ptr: pointer to byte block
- * @size: number of bytes to assign a zero
- *
- * Return: void
- */
-
-void setm_zero(char *ptr, int size)
-{
-	int i = 0;
-
-	for (i = 0; i < size; i++)
-	{
-		ptr[i] = '\0';
-	}
+	return (arr);
 }
