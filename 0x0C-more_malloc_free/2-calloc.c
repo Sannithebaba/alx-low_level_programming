ecfad1b64d89a2fc0450c09ac8e3acd0228945d5
Sannithebaba <mubaraksanniajiferuke@gmail.com>
2022-10-06 12:43:20 -0700

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements in array
+ * @size: number of bytes per element
+ *
+ * Return: a void pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = NULL;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	setm_zero((char *)ptr, size * nmemb);
+
+	return (ptr);
+}
+
+
+/**
+ * setm_zero - set memory on zero
+ * @ptr: pointer to byte block
+ * @size: number of bytes to assign a zero
+ *
+ * Return: void
+ */
+
+void setm_zero(char *ptr, int size)
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = '\0';
+	}
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,2 +1,3 @@
 1 - a function that allocates memory using malloc
 2 - a function that concatenates two strings
+3 - a function that allocates memory for an array, using malloc
